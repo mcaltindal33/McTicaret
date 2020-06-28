0d20a10730b54e22ff5865af76dbd166073c7593
Mustafa Çağrı ALTINDAL <mcaltindal@cagrialtindal.com.tr>
2020-06-29 00:52:16 +0300

    ....

--- McTicaret.Win/WinApplication.cs
+++ McTicaret.Win/WinApplication.cs
@@ -38,27 +38,8 @@ namespace McTicaret.Win {
             }
         }
         private void McTicaretWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
-//#if EASYTEST
             e.Updater.Update();
             e.Handled = true;
-//#else
-//            if (System.Diagnostics.Debugger.IsAttached) {
-//                e.Updater.Update();
-//                e.Handled = true;
-//            }
-//            else {
-//				string message = "The application cannot connect to the specified database, " +
-//					"because the database doesn't exist, its version is older " +
-//					"than that of the application or its schema does not match " +
-//					"the ORM data model structure. To avoid this error, use one " +
-//					"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
-
-//				if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
-//					message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
-//				}
-//				throw new InvalidOperationException(message);
-//            }
-//#endif
         }
     }
 }
